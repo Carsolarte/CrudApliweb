2fd7efb296f322c5c25e857bf6a53e4c4fcad655
Andrés Felipe Ocampo <89326187+AndresFelipeO@users.noreply.github.com>
2023-11-11 21:49:46 -0500

    Crud

--- CrudDeptEmp.php
+++ CrudDeptEmp.php
@@ -1,10 +1,9 @@
 <?php
-
 include('db.php');
 //create
 if (isset($_POST['bt_guardar_dep_emp'])) {
   $idDepto = $_POST['dept_no'];
-  $idemp = $_POST['emp_np'];
+  $idemp = $_POST['emp_no'];
   $fechaActual = date('Y-m-d');
   if ($idDepto != '' and $idDepto != null and $idemp != '' and $idemp != null) {
     $query = "INSERT INTO dept_emp(emp_no, dept_no, from_date, to_date) VALUES ( '$idemp', '$idDepto', '$fechaActual', '2023-12-31')";
@@ -13,7 +12,6 @@ if (isset($_POST['bt_guardar_dep_emp'])) {
       die("Query Failed.");
     }
     echo "saved";
-
     $_SESSION['message'] = 'Departamento guardado correctamentex';
     $_SESSION['message_type'] = 'success';
   }
@@ -21,46 +19,39 @@ if (isset($_POST['bt_guardar_dep_emp'])) {
 //update
 if (isset($_POST['bt_update_dep_emp'])) {
   $idDepto = $_POST['dept_no'];
-  $idemp = $_POST['emp_np'];
-  $fechaActual = date('Y-m-d');
+  $idemp = $_POST['emp_no'];
+  $fechaActual = $_POST['nuevaFecha'];
+  echo "<script>";
+  echo "console.log('idDepto:', '$idDepto');";
+  echo "console.log('name:', '$name');";
+  echo "console.log('fechaActual:', '$fechaActual');";
+  echo "</script>";
   if ($idDepto != '' and $idDepto != null and $idemp != '' and $idemp != null) {
-     // Actualiza la fecha to_date en la tabla dept_emp
-     $queryUpdateDeptEmp = "UPDATE dept_emp SET to_date = '$fechaActual' WHERE emp_no = '$idemp' AND dept_no = '$idDepto'";
-     $resultUpdateDeptEmp = mysqli_query($conexion, $queryUpdateDeptEmp);
-     if (!$resultUpdateDeptEmp) {
-         die("Query Failed: " . mysqli_error($conexion));
-     }
- 
-     echo "updated";
-    
-    
-    $query = "INSERT INTO dept_emp(emp_no, dept_no, from_date, to_date) VALUES ( '$idemp', '$idDepto', '$fechaActual', '2023-12-31')";
-    $result = mysqli_query($conexion, $query);
-    if (!$result) {
-      die("Query Failed.");
+    // Actualiza la fecha to_date en la tabla dept_emp
+    $queryUpdateDeptEmp = "UPDATE dept_emp SET to_date = '$fechaActual' WHERE emp_no = '$idemp' AND dept_no = '$idDepto'";
+    $resultUpdateDeptEmp = mysqli_query($conexion, $queryUpdateDeptEmp);
+    if (!$resultUpdateDeptEmp) {
+      die("Query Failed: " . mysqli_error($conexion));
     }
-    echo "saved";
+    echo "updated";
     $_SESSION['message'] = 'Departamento actualizado correctamente';
     $_SESSION['message_type'] = 'success';
-  
   }
 }
 //delete
 if (isset($_POST['bt_delete_dep_emp'])) {
   $idDepto = $_POST['dept_no'];
-  $idemp = $_POST['emp_np'];
+  $idemp = $_POST['emp_no'];
   $fechaActual = date('Y-m-d');
   if ($idDepto != '' and $idDepto != null and $idemp != '' and $idemp != null) {
     $queryDeleteDeptEmp = "DELETE FROM dept_emp WHERE emp_no = '$idemp' AND dept_no = '$idDepto'";
     $resultDeleteDeptEmp = mysqli_query($conexion, $queryDeleteDeptEmp);
     if (!$resultDeleteDeptEmp) {
-        die("Query Failed: " . mysqli_error($conexion));
+      die("Query Failed: " . mysqli_error($conexion));
     }
     echo "deleted";
-
     $_SESSION['message'] = 'Departamento eliminado correctamente';
     $_SESSION['message_type'] = 'success';
-
   }
 }
 header('Location: visualizarDepto.php');
