7abf320037e97ab84cf67016e86d3f8cce1eff23
Carsolarte <carsolarte@unicauca.edu.co>
2023-11-11 20:20:41 -0500

    modal

--- visualizarDepto.php
+++ visualizarDepto.php
@@ -43,11 +43,11 @@ include("db.php");
                                 </td>
                                 <td>
                                     <?php echo $fila['first_name'] ?>
-                               <td>
+                                <td>
                                     <?php echo $fila['last_name'] ?>
-                               
-                               <?php } ?>
-                               
+
+                                <?php } ?>
+
                     </tbody>
                 </table>
             </div>
@@ -83,23 +83,23 @@ include("db.php");
 </div>
 
 <div class="container p-4">
-<div class="col-md-4">
-    <div class="row">
-        <div class="card card-body">
-            <form action="addDepto.php" method="POST">
-                <div class="form-group mb-3">
-                    <input type="text" name="dept_no" class="form-control" placeholder="N° Departameto" autofocus>
-                </div>
-                <div class="form-group mb-3">
-                    <input type="text" name="emp_np" class="form-control" placeholder="N° Empleado" autofocus>
-                </div>
-                <div class="d-grid">
-                    <input name="bt_guardar_dep_emp" type="submit" class="btn btn-success" value="Guardar">
-                </div>
-            </form>
+    <div class="col-md-4">
+        <div class="row">
+            <div class="card card-body">
+                <form action="addDepto.php" method="POST">
+                    <div class="form-group mb-3">
+                        <input type="text" name="dept_no" class="form-control" placeholder="N° Departameto" autofocus>
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="text" name="emp_np" class="form-control" placeholder="N° Empleado" autofocus>
+                    </div>
+                    <div class="d-grid">
+                        <input name="bt_guardar_dep_emp" type="submit" class="btn btn-success" value="Guardar">
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 </div>
 </div>
 
@@ -132,12 +132,41 @@ include("db.php");
                     <td>
                         <?php echo $fila['dept_name'] ?>
                     <td>
-                        <a class="btn btn-secondary"
-                            href="editar_tarea.php?id=<?php echo $fila['emp_no'] . '_' . $fila['dept_no']; ?>">Editar</a>
+                        <button class="btn btn-secondary editar-btn" data-toggle="modal" data-target="#editarModal"
+                            data-emp-no="<?php echo $fila['emp_no'];
+                            ?>" data-dept-no="<?php echo $fila['dept_no']; ?>">Editar</button>
                         <a class="btn btn-danger"
                             href="eliminar_tarea.php?id=<?php echo $fila['emp_no'] . '_' . $fila['dept_no']; ?>">Eliminar</a>
                     </td>
                 <?php } ?>
+               
+                <div class="modal" tabindex="-1"id="editarModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Editar Fecha</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                            <form id="editarForm">
+                                    <div class="form-group">
+                                        <label for="nuevaFecha">Nueva Fecha:</label>
+                                        <input type="date" class="form-control" id="nuevaFecha" name="nuevaFecha">
+                                    </div>
+                                    <input type="hidden" id="empNoHidden" name="empNoHidden" value="">
+                                    <input type="hidden" id="deptNoHidden" name="deptNoHidden" value="">
+                                    <button type="submit" class="btn btn-primary">Guardar Cambios</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Guardar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
         </tbody>
     </table>
 </div>
